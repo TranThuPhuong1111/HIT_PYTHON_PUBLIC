0ff36b7f8ae4eb8977702787db619437b9350499
TranThuPhuong1111 <thuphuongchan1@gmail.com>
2024-08-05 19:48:27 +0700

    test1

--- cau2.py
+++ cau2.py
@@ -1,21 +1,5 @@
-setA = set()
-setB = set()
-nA = int(input())
-nB = int(input())
-print()
-for i in range(nA):
-    setA.add(int(input()))
-print()
-for i in range(nB):
-    setB.add(int(input()))
-lstDS = []
-lstDS2 = []
-for i in setA:
-    set_tmp = {i}
-    if set_tmp.issubset(setB) == True:
-        lstDS.append(i)
-    else:
-        lstDS2.append(i)
-    set_tmp.clear()
-print(lstDS)
-print(lstDS2)
+s = int(input())
+
+def buocdem(s):
+    return s // 3 + (bool)(s % 3)
+print(buocdem(s))
--- cau4.py
+++ cau4.py
@@ -1,16 +1,7 @@
-n = 5
-setA = {1 , 2, 3, 4, 5}
-maxSum = 7
-
-setNew = sorted(setA)
-
-setCon = set()
-sum = 0
-
-for i in setNew:
-    if sum + i <= maxSum:
-        setCon.add(i)
-        sum += i
-    else:
-        break
-print(setCon)
+str = input()
+result = []
+for char in str:
+    if char == " ":
+        continue
+    result.append(char)
+print(set(list(result)))
